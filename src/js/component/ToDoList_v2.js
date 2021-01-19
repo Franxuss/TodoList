9699409766558d341b44dc7f05fb4c0570cc59ae
Franxuss <franxuss_gp@hotmail.com>
2021-01-19 15:33:43 +0000

    Añadido style

--- src/js/component/ToDoList_v2.js
+++ src/js/component/ToDoList_v2.js
@@ -30,34 +30,52 @@ export const ToDoList_v2 = () => {
 		setItems(updatedItems);
 	};
 
+	const textInput = () => {
+		var textoInput = "";
+		if (items.length === 0) {
+			textoInput = "Añade tu primera tarea";
+		} else {
+			textoInput = "Añade más tareas...";
+		}
+		return textoInput;
+	};
+
+	var itemsLeft = items.length;
+
 	return (
 		<div className="container">
-			<h1>todos</h1>
-			<form type="submit" onSubmit={addItem}>
-				<input
-					id="input"
-					className="form-control"
-					placeholder="Introduzca una tarea..."
-					type="text"
-					onChange={inputTask}
-					value={input}
-				/>
-			</form>
-			<ul>
-				{items.map(input => {
-					return (
-						<li key={input.id}>
-							{input.text}
-							<button
-								type="button"
-								className="close"
-								onClick={() => deleteItem(input.id)}>
-								&times;
-							</button>
-						</li>
-					);
-				})}
-			</ul>
+			<div className="tittle text-center">
+				<h1>todos</h1>
+				<div className="cardList">
+					<form type="submit" onSubmit={addItem}>
+						<input
+							className="inputText"
+							placeholder={textInput()}
+							type="text"
+							onChange={inputTask}
+							value={input}
+						/>
+					</form>
+					<ul className="list-group list-group-flush">
+						{items.map(input => {
+							return (
+								<li className="list-group-item" key={input.id}>
+									{input.text}
+									<button
+										type="button"
+										className="close"
+										onClick={() => deleteItem(input.id)}>
+										&times;
+									</button>
+								</li>
+							);
+						})}
+						<div id="itemsLeft" className="text-left">
+							{itemsLeft} items left
+						</div>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
